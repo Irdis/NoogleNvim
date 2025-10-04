2ec27a8d63f374f29b842ef772d5693b51290e85
Ivan Novitskii <ivannovitskii@gmail.com>
2025-10-04 22:22:03 +0100

    test

--- net/Program.cs
+++ net/Program.cs
@@ -70,6 +70,7 @@ public class Program
 
     public static async Task Main(string[] args)
     {
+        Console.WriteLine("Hello world");
         if (!ParseArgs(args, out var noogleArgs))
         {
             return;
